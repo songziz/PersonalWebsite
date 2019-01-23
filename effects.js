7aa4cf252e8f31d278df36dff1efbf3a2b11e2c0
Zizhen Song <zizhensong@gmail.com>
2019-01-23 01:48:05 -0800

    Fixed Bug with Scroll-On header

--- effects.js
+++ effects.js
@@ -2,18 +2,25 @@
 
 // When the user scrolls the page, execute myFunction 
 window.onscroll = function() {myFunction()};
-
+window.onresize = function() {updateBarLoc()};
 // Get the header
-var header = document.getElementById("myHeader");
 
-// Get the offset position of the navbar
+var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
+// Get the offset position of the navbar
+
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset > (sticky)) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
+
+  	if (window.pageYOffset > sticky) {
+    	header.classList.add("sticky");
+  	} else {
+    	header.classList.remove("sticky");
   }
+}
+
+function updateBarLoc(){
+	header = document.getElementById("myHeader");
+	sticky = header.offsetTop;
 }
